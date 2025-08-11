72960608e82b4bd362a9a3d8ea43e17d24de4bda
shruti01-dev <shrutitiwari48201@gmail.com>
2025-08-11 23:16:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,120 @@
+const form = document.querySelector("form");
+eField = form.querySelector(".email"),
+    eInput = eField.querySelector("input"),
+    pField = form.querySelector(".password"),
+    pInput = pField.querySelector("input");
+
+form.onsubmit = (e) => {
+    e.preventDefault(); //preventing from form submitting
+    //if email and password is blank then add shake class in it else call specified function
+    (eInput.value == "") ? eField.classList.add("shake", "error"): checkEmail();
+    (pInput.value == "") ? pField.classList.add("shake", "error"): checkPass();
+
+    setTimeout(() => { //remove shake class after 500ms
+        eField.classList.remove("shake");
+        pField.classList.remove("shake");
+    }, 500);
+
+    eInput.onkeyup = () => { checkEmail(); } //calling checkEmail function on email input keyup
+    pInput.onkeyup = () => { checkPass(); } //calling checkPassword function on pass input keyup
+
+    function checkEmail() { //checkEmail function
+        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; //pattern for validate email
+        if (!eInput.value.match(pattern)) { //if pattern not matched then add error and remove valid class
+            eField.classList.add("error");
+            eField.classList.remove("valid");
+            let errorTxt = eField.querySelector(".error-txt");
+            //if email value is not empty then show please enter valid email else show Email can't be blank
+            (eInput.value != "") ? errorTxt.innerText = "Enter a valid email address": errorTxt.innerText = "Email can't be blank";
+        } else { //if pattern matched then remove error and add valid class
+            eField.classList.remove("error");
+            eField.classList.add("valid");
+        }
+    }
+
+    function checkPass() { //checkPass function
+        if (pInput.value == "") { //if pass is empty then add error and remove valid class
+            pField.classList.add("error");
+            pField.classList.remove("valid");
+        } else { //if pass is empty then remove error and add valid class
+            pField.classList.remove("error");
+            pField.classList.add("valid");
+        }
+    }
+
+    //if eField and pField doesn't contains error class that mean user filled details properly
+    if (!eField.classList.contains("error") && !pField.classList.contains("error")) {
+        window.location.href = form.getAttribute("action"); //redirecting user to the specified url which is inside action attribute of form tag
+    }
+}
+
+const form = document.querySelector("form"),
+    emailField = form.querySelector(".email-field"),
+    emailInput = emailField.querySelector(".email"),
+    passField = form.querySelector(".create-password"),
+    passInput = passField.querySelector(".password"),
+    cPassField = form.querySelector(".confirm-password"),
+    cPassInput = cPassField.querySelector(".cPassword");
+
+// Email Validtion
+function checkEmail() {
+    const emaiPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (!emailInput.value.match(emaiPattern)) {
+        return emailField.classList.add("invalid"); //adding invalid class if email value do not mathced with email pattern
+    }
+    emailField.classList.remove("invalid"); //removing invalid class if email value matched with emaiPattern
+}
+
+// Hide and show password
+const eyeIcons = document.querySelectorAll(".show-hide");
+
+eyeIcons.forEach((eyeIcon) => {
+    eyeIcon.addEventListener("click", () => {
+        const pInput = eyeIcon.parentElement.querySelector("input"); //getting parent element of eye icon and selecting the password input
+        if (pInput.type === "password") {
+            eyeIcon.classList.replace("bx-hide", "bx-show");
+            return (pInput.type = "text");
+        }
+        eyeIcon.classList.replace("bx-show", "bx-hide");
+        pInput.type = "password";
+    });
+});
+
+// Password Validation
+function createPass() {
+    const passPattern =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if (!passInput.value.match(passPattern)) {
+        return passField.classList.add("invalid"); //adding invalid class if password input value do not match with passPattern
+    }
+    passField.classList.remove("invalid"); //removing invalid class if password input value matched with passPattern
+}
+
+// Confirm Password Validtion
+function confirmPass() {
+    if (passInput.value !== cPassInput.value || cPassInput.value === "") {
+        return cPassField.classList.add("invalid");
+    }
+    cPassField.classList.remove("invalid");
+}
+
+// Calling Funtion on Form Sumbit
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); //preventing form submitting
+    checkEmail();
+    createPass();
+    confirmPass();
+
+    //calling function on key up
+    emailInput.addEventListener("keyup", checkEmail);
+    passInput.addEventListener("keyup", createPass);
+    cPassInput.addEventListener("keyup", confirmPass);
+
+    if (!emailField.classList.contains("invalid") &&
+        !passField.classList.contains("invalid") &&
+        !cPassField.classList.contains("invalid")
+    ) {
+        location.href = form.getAttribute("action");
+    }
+});
